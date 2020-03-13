5fbb2a5b3c24cf56eac293f3e5d7db64ca00524f
pedro <pedroraft@gmail.com>
2020-03-13 12:26:21 -0300

    write barrels and file watcher

--- src/cli.ts
+++ src/cli.ts
@@ -1,20 +1,131 @@
-import nsfw from 'nsfw';
-import path from 'path';
 import fastGlob from 'fast-glob';
 import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import prettier from 'prettier';
+import { Project } from 'ts-morph';
+import nsfw, { ActionType } from 'nsfw';
 
 type exportType = 'named' | 'default';
 type fileExportType = { [file: string]: exportType };
-type indexInfo = { [index: string]: fileExportType };
+const project = new Project();
+const CONCURRENCY = os.cpus().length;
 
 // const mockFolder = path.join(process.cwd(), 'testfolder');
-const mockFolder = '/home/pedroraft/Documentos/v2/src';
+const mockFolder = '/Users/Pedroraft/Documents/v2/src';
 console.log(mockFolder);
 
 const IGNORE_MULTI_DOT: string[] | boolean | undefined = true;
 
 export const run = () => {
-  console.log(buildIndexTree(getFiles()));
+  project.addSourceFilesAtPaths(path.join(mockFolder, '**/*.{ts,tsx}'));
+  buildIndexes();
+  // await indexes write
+  setTimeout(
+    () =>
+      nsfw(
+        mockFolder,
+        events => {
+          const safeEvents = events.filter(
+            (e: any) =>
+              (e.file === 'index.ts' && e.action !== ActionType.MODIFIED) ||
+              e.file !== 'index.ts',
+          );
+          if (safeEvents.find((e: any) => e.file === 'index.ts'))
+            buildIndexes();
+          else {
+            const newIndex = buildIndexTree(getFiles());
+            const alteredIndexes: string[] = [];
+            safeEvents
+              .map((e: any) => path.join(e.directory, e.file))
+              .forEach(f =>
+                Object.keys(newIndex).forEach(key => {
+                  if (newIndex[key].includes(f)) alteredIndexes.push(key);
+                }),
+              );
+            alteredIndexes
+              .filter((value, index, self) => self.indexOf(value) === index)
+              .forEach(key => writeIndex(key, newIndex[key]));
+          }
+        },
+        { debounceMS: 200 },
+      ).then(watcher => watcher.start()),
+    200,
+  );
+};
+
+const buildIndexes = () => {
+  const files = getFiles();
+  const indexes = buildIndexTree(files);
+  Object.keys(indexes).forEach(key => writeIndex(key, indexes[key]));
+};
+
+const writeIndex = (indexPath: string, files?: string[]) => {
+  if (!files?.length) return;
+  const indexDir = path.dirname(indexPath);
+  const defaultExports = files
+    .filter(f => fileHasDefault(f))
+    .map(f => getDefaultExportLine(indexDir, f))
+    .join('\n');
+  const namedFiles = files.filter(f => !fileHasDefault(f));
+  const namedImports = namedFiles
+    .map(f => getNamedImportLine(path.dirname(indexPath), f))
+    .join('\n');
+  const namedExports =
+    namedFiles?.length > 0
+      ? namedFiles.reduce((collection, current, index) => {
+          let text = '';
+          if (index === 0) text += 'export {';
+          text += `${collection}\n  ${getCamelizedName(current)},`;
+          if (index === namedFiles.length - 1) text += `\n};`;
+          return text;
+        }, '')
+      : '';
+  const content = [namedImports, '', namedExports, '', defaultExports, ''].join(
+    '\n',
+  );
+  // const pretty = prettier.format(content);
+  fs.writeFileSync(indexPath, content);
+};
+
+const getNameWithoutExt = (filePath: string) =>
+  path.basename(filePath).split('.')[0];
+
+const camelize = (str: string) =>
+  str
+    .replace(/-/g, ' ')
+    .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, match =>
+      +match === 0 ? '' : match.toUpperCase(),
+    );
+
+const getRelative = (indexDir: string, filePath: string) =>
+  path
+    .relative(indexDir, filePath)
+    .replace('.tsx', '')
+    .replace('.ts', '');
+
+const getCamelizedName = (filePath: string) =>
+  camelize(getNameWithoutExt(filePath));
+
+const getDefaultExportLine = (indexDir: string, filePath: string) =>
+  `export { default as ${getCamelizedName(filePath)} } from './${getRelative(
+    indexDir,
+    filePath,
+  )}';`;
+
+const getNamedImportLine = (indexDir: string, filePath: string) =>
+  `import * as ${getCamelizedName(filePath)} from './${getRelative(
+    indexDir,
+    filePath,
+  )}';`;
+
+const fileHasDefault = (filePath: string) => {
+  try {
+    const exports = project.getSourceFile(filePath).getExportedDeclarations();
+    return exports.has('default');
+  } catch (e) {
+    return false;
+  }
 };
 
 const getFiles = () => {
@@ -22,7 +133,6 @@ const getFiles = () => {
     dot: false,
     ignore: ['**/node_modules/*'],
   });
-  // console.log(entries);
   return filterMultidot(entries);
 };
 
@@ -54,15 +164,18 @@ const buildIndexTree = (files: string[]) => {
       const flatIndex = getFlatIndexes();
       indexTree = {
         ...indexTree,
-        [indexFile]: files.filter(
-          f =>
-            // in the same path
-            f.startsWith(path.dirname(indexFile)) &&
-            // ins't in other index
-            flatIndex.every(x => x !== f) &&
-            // removing it self
-            f !== indexFile,
-        ),
+        [indexFile]: files
+          .filter(
+            f =>
+              // removing it self
+              f !== indexFile &&
+              // in the same path
+              f.startsWith(path.dirname(indexFile)) &&
+              // ins't in other index
+              flatIndex.every(x => x !== f),
+          )
+          // alphabetically order
+          .sort(),
       };
     });
   return indexTree;
